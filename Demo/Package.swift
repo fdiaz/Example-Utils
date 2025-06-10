f43c152155d1b31a13cbc4ade08ebad3b5093247
Francisco Díaz <patchdiaz@gmail.com>
2025-06-09 22:49:02 -0300

    Working example

--- Demo/Package.swift
+++ Demo/Package.swift
@@ -4,17 +4,18 @@ import PackageDescription
 let package = Package(
     name: "UtilsDemo",
     products: [
-          .executable(name: "UtilsDemo", targets: ["UtilsDemo"]),
+        .executable(name: "UtilsDemo", targets: ["UtilsDemo"]),
+    ],
     dependencies: [
         .package(path: "../"),
-        .package(url: "https://github.com/fdiaz/Example-SupportingFiles.git", from: "1.0.0")
+        .package(url: "https://github.com/fdiaz/Example-SupportingFiles.git", from: "1.0.0"),
     ],
     targets: [
         .executableTarget(
             name: "UtilsDemo",
             dependencies: [
-                "Utils",
-                .product(name: "SupportingFiles", package: "Example-SupportingFiles")
+                .product(name: "Utils", package: "Example-Utils"),
+                .product(name: "SupportingFiles", package: "Example-SupportingFiles"),
             ]
         )
     ]
